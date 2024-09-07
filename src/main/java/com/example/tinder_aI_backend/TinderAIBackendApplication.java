9f89b7035dc8f29de04630df87868abb638434a8
Rishav <rishavpramanik17@gmail.com>
2024-09-07 16:07:06 +0530

    basic initializer app working

---
+++ src/main/java/com/example/tinder_aI_backend/TinderAIBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.tinder_aI_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TinderAIBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TinderAIBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/tinder_aI_backend/TinderAIBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tinder_aI_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TinderAIBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
